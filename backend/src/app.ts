cfac85ee7be2a4d4a2e88c953859c490546ebd1b
matt087 <emontenegroc@est.ups.edu.ec>
2026-07-22 13:15:49 -0500

    chore: Configuracion inicial del proyecto

---
+++ backend/src/app.ts
@@ -0,0 +1,17 @@
+import express, {Request, Response} from "express";
+
+const app = express();
+const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
+
+app.use(express.json());
+
+app.get("/", (_req: Request, res: Response)=>{
+    res.json({
+        status: "ok",
+        timestamp: new Date().toISOString(),
+    });
+});
+
+app.listen(PORT, () =>{
+    console.log(`Servidor backend corriendo en http://localhost:${PORT}`);
+});
